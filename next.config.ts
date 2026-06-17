4595e3feadb4a5f172e78bfa64d81c161515bb3b
angerr <artemkozaderov82174@gmail.com>
2026-06-17 12:08:42 +0300

    Fix typescript error in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,10 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   allowedDevOrigins: ['172.18.0.1', 'localhost'],
-  devIndicators: {
-    buildActivity: false,
-    appIsrStatus: false,
-  },
+
 };
 
 export default nextConfig;
